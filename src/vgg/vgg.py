95dc20a1e2bb725887fa4bc4a66a3e4eeab4c406
Alexey Zavalskiy <papyas12@hotmail.com>
2022-04-11 19:23:52 +0300

    Initial commit

---
+++ src/vgg/vgg.py
@@ -0,0 +1,53 @@
+from pyexpat import model
+from numpy import append
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+VGG16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+class VGG_net(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000) -> None:
+        super(VGG_net, self).__init__()
+        self.in_channels = in_channels
+        self.conv_layers = self.create_conv_layers(VGG16)
+        self.fcn = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fcn(x)
+        return x
+
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for x in architecture:
+            if type(x) == int:
+                out_channels = x
+                layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                    kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+                                nn.BatchNorm2d(x),
+                                nn.ReLU()]
+                in_channels = x
+            elif x == 'M':
+                layers.append(nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)))
+        return nn.Sequential(*layers)
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+x = torch.rand(1, 3, 224, 224).to(device)
+model = VGG_net(in_channels=3, num_classes=1000).to(device)
+
+print(model(x).shape)
