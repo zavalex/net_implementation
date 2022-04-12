b6c28f5ce1331381a0e1592835dfe110e5b4e511
Alexey Zavalskiy <papyas12@hotmail.com>
2022-04-12 20:55:47 +0300

    PEP8

--- src/vgg/vgg.py
+++ src/vgg/vgg.py
@@ -1,14 +1,9 @@
-from pyexpat import model
-from numpy import append
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
 
-VGG16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+VGG16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+         'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
 
 class VGG_net(nn.Module):
     def __init__(self, in_channels=3, num_classes=1000) -> None:
@@ -36,13 +31,17 @@ class VGG_net(nn.Module):
         for x in architecture:
             if type(x) == int:
                 out_channels = x
-                layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                                    kernel_size=(3,3), stride=(1,1), padding=(1,1)),
-                                nn.BatchNorm2d(x),
-                                nn.ReLU()]
+                layers += [
+                    nn.Conv2d(
+                        in_channels=in_channels, out_channels=out_channels,
+                        kernel_size=(3, 3),
+                        stride=(1, 1),
+                        padding=(1, 1)),
+                    nn.BatchNorm2d(x),
+                    nn.ReLU()]
                 in_channels = x
             elif x == 'M':
-                layers.append(nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)))
+                layers.append(nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)))
         return nn.Sequential(*layers)
 
 
